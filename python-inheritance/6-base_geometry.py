741fe061d887279f4a9b8955183b3f5a71c3312b
Ikewa <i.kewa@alustudent.com>
2023-07-27 07:31:41 -0700

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a class BaseGeometry (based on 5-base_geometry.py)."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
