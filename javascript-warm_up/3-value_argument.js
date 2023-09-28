8b35074a9f1b0798f51d229f2877c094babd7ba5
Ikewa <i.kewa@alustudent.com>
2023-09-28 06:44:55 -0700

    task 3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined)
+// Check if the first argument exists
+if (process.argv[2] === undefined) {
+  // No argument passed
   console.log('No argument');
 } else {
+  // Print the first argument
   console.log(process.argv[2]);
 }
